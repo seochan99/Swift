1fd6d2f9f26ef92aafe58089ff11515340b22fc6
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-06-26 22:32:03 +0900

    ㄴㄴ

---
+++ ClassesDemo/ClassesDemo/main.swift
@@ -0,0 +1,15 @@
+// init
+let skeleton = Enemy()
+
+//print(skeleton.health)
+//skeleton.move()
+//skeleton.attack()
+
+let dragon = Dragon()
+dragon.wingSpan = 5 // dragon에게만
+dragon.attackStrength = 15
+dragon.talk(speech: "MY NAME IS CHAN!!!!")
+
+dragon.move()
+dragon.attack()
+ㄴ
