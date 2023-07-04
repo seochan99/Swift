de47bdc7b41a022b1e1fc5ce919fa7f78714a9fb
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-07-04 10:17:41 +0900

    feat : ch02-12

--- myAlbum/myAlbum/ViewController.swift
+++ myAlbum/myAlbum/ViewController.swift
@@ -8,14 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func showAlert(_ sender: Any) {
+        let message = "가격은 \(currentValue)입니다."
         // alert, 알림창 만들기
-        let alert = UIAlertController(title: "Hello", message: "My First App", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
         
         
         // action, 버튼 클릭시 핸들 가능, 지금은 nil
@@ -26,17 +29,15 @@ class ViewController: UIViewController {
         
         // 이를 통해 alert를 띄우는 작업을 함
         present(alert,animated: true,completion: nil)
-    }
-    
-    @IBAction func haha(_ sender: Any) {
-        let alert = UIAlertController(title: "웃지마 ㅋ", message: "웃지믈르고", preferredStyle: .alert)
-        let action = UIAlertAction(title:"K",style:.default, handler:nil)
         
-        alert.addAction(action)
-        present(alert,animated: true,completion: nil)
+        // random숫자 가져오는 법, UInt32 Type
+        let randomPrice = arc4random_uniform(10000)+1
+        
+        //UInt32이라서 Int로 변환
+        currentValue = Int(randomPrice)
     }
     
-    
+
 
 }
 
