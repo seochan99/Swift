fe40f9500cb2d12a517d893dcf3322ef5004a1ed
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-07-10 02:23:43 +0900

    feat : view속성 애니메이션

--- BountyList/BountyList/DetailViewController.swift
+++ BountyList/BountyList/DetailViewController.swift
@@ -52,8 +52,15 @@ class DetailViewController: UIViewController {
     
     private func prepareAnimation(){
         // 밖에 내보내기
-        nameLabelCenterX.constant = view.bounds.width
-        bountyLabelCenterX.constant = view.bounds.width
+//        nameLabelCenterX.constant = view.bounds.width
+//        bountyLabelCenterX.constant = view.bounds.width
+        
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
         
     }
     // 들어오기
@@ -72,13 +79,31 @@ class DetailViewController: UIViewController {
 //            self.view.layoutIfNeeded()
 //        }, completion: nil)
         
-        // 스프링 효과
-        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2,options: .allowUserInteraction, animations: {
-            self.view.layoutIfNeeded()
+//        // 스프링 효과
+//        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2,options: .allowUserInteraction, animations: {
+//            self.view.layoutIfNeeded()
+//        },completion: nil)
+//
+//        // 카드 뒤집기
+//        UIView.transition(with: imgView, duration: 0.3, options:.transitionFlipFromLeft, animations: nil,completion: nil)
+        
+                UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2,options: .allowUserInteraction, animations: {
+                    self.nameLabel.transform = CGAffineTransform.identity
+                    self.nameLabel.alpha = 1
+                },completion: nil)
+        
+        UIView.animate(withDuration: 1, delay: 0.3, usingSpringWithDamping: 0.6, initialSpringVelocity: 2,options: .allowUserInteraction, animations: {
+            self.bountyLabel.transform = CGAffineTransform.identity
+            self.bountyLabel.alpha = 1
         },completion: nil)
         
-        // 카드 뒤집기
-        UIView.transition(with: imgView, duration: 0.3, options:.transitionFlipFromLeft, animations: nil,completion: nil)
+                UIView.transition(with: imgView, duration: 0.3, options:.transitionFlipFromLeft, animations: nil,completion: nil)
+        
+        // 뷰 속성 animation
+        
+        
+        
+        
     }
     
     
